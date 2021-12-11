354447956c6f4a7f49227b4599a39ed754f2c951
William Philippe <wplb@ic.ufal.br>
2021-12-11 09:23:43 -0300

    feat: read from cmdl

--- compiler/src/main.ts
+++ compiler/src/main.ts
@@ -1,12 +1,25 @@
 /* eslint-disable no-console */
 import { FileHandler } from './fileHandler';
 import { Scanner } from './scanner';
+import * as readline from 'readline';
 
 const scanner = new Scanner();
 
-const file = new FileHandler(
-  (line, lineCount) => scanner.processLine(line, lineCount),
-  './programas/helloWorld.anw',
-);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-file.readLineByLine();
+rl.question('Qual o caminho do arquivo? ', function(path) {
+  console.info();
+  console.info();
+  console.info();
+  const file = new FileHandler(
+    (line, lineCount) => scanner.processLine(line, lineCount),
+    path,
+  );
+
+  file.readLineByLine();
+
+  rl.close();
+});
