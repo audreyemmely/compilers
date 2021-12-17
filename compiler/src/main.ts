70912962bbcc26f3d175a63c3d6b596c7250aab1
William Philippe <wplb@ic.ufal.br>
2021-12-17 17:36:18 -0300

    feat: compiler

--- compiler/src/main.ts
+++ compiler/src/main.ts
@@ -1,30 +1,19 @@
 /* eslint-disable no-console */
 import { FileHandler } from './fileHandler';
 import { Scanner } from './scanner';
-import * as readline from 'readline';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const scanner = new Scanner();
 
-rl.question('Qual o caminho do arquivo? ', function(path) {
-  console.info();
-  console.info();
-  console.info();
+const file = new FileHandler(
+  (line, lineCount) => scanner.processLine(line, lineCount),
+  process.argv[2],
+);
 
-  const scanner = new Scanner();
+// path = './programas/helloWorld.anw';
+// path = './programas/shellSort.anw';
+// path = './programas/fibonacci.anw';
 
-  path = './programas/helloWorld.anw';
-  // path = './programas/shellSort.anw';
-  // path = './programas/fibonacci.anw';
+file.readLineByLine();
 
-  const file = new FileHandler(
-    (line, lineCount) => scanner.processLine(line, lineCount),
-    path,
-  );
-
-  file.readLineByLine();
-
-  rl.close();
-});
+// console.info('process');
+// console.info(process.argv);
